fd5a8ec8f56c92015be7b9563f92305fd389114d
Mukul Gera <gera.mukul304@gmail.com>
2023-04-02 13:31:16 +0530

    first commit

---
+++ src/test/java/com/qa/test/Test1.java
@@ -0,0 +1,18 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void test1() {
+		System.out.println("This is method 1");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("This is method 2");
+		System.out.println("");
+	}
+
+}
