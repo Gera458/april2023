6d5b7b0fee5282d4480e295b45632cb9eb2c204d
Gera458 <45786840+Gera458@users.noreply.github.com>
2023-04-11 17:25:23 +0530

    Update Test1.java

--- src/test/java/com/qa/test/Test1.java
+++ src/test/java/com/qa/test/Test1.java
@@ -17,13 +17,13 @@ public class Test1 {
 
 	@Test
 	public void test3() {
-		System.out.println("This is method 2");
+		System.out.println("This is method 3");
 		System.out.println("");
 	}
 
 	@Test
 	public void test4() {
-		System.out.println("This is method 2");
+		System.out.println("This is method 4");
 		System.out.println("");
 	}
 
